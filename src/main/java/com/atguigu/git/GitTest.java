a7ce443d54ba56987d21c4da4b9583a6f28475f1
wensiyuan980507 <1092504283@qq.com>
2022-07-21 22:09:07 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git5");
         System.out.println("hello git6");
         System.out.println("hello git7");
+        System.out.println("hotfix test");
     }
 }
